15df7355f4cd665dab328e8218cc4f3704ec3e86
JayHuang709 <huangweijie709@hotmail.com>
2016-05-09 07:25:46 +0800

    Update RProgrammingAssignment2.R

--- RProgrammingAssignment2.R
+++ RProgrammingAssignment2.R
@@ -1,17 +1,24 @@
-makeCacheMatirx<-function(x=matrix()){
+##This function contain four part
+makeCacheMatrix<-function(x=matrix()){
   inv<-NULL
+  
+  ##set matrix
   set<-function(y){
     x<<-y
     inv<<-NULL
   }
+  ##get matrix
   get<-function() x
+  ##set matrix inverse
   setinverse<-function(inverse)inv<<-inverse
+  ##get matrix inverse
   getinverse<-function()inv
   list(set=set,get=get,
        getinverse=getinverse,
        setinverse=setinverse)
 }
 
+#This function cach the inverse of matrix,both get or set
 cacheSovle<-function(x){
   inv<-x$getinverse()
   if(!is.null(inv)){
@@ -19,6 +26,7 @@ cacheSovle<-function(x){
     return(inv)
   }
   data<-x$get()
+  ##calculate the inverse of matrix
   inv<-sovle(data)
   x$setinverse(inv)
   inv
